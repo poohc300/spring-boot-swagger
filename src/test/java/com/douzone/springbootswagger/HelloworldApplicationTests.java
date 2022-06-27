da73bac604ddc78d1faa41b53d05407874d66d0e
poohc300 <poohc300@gmail.com>
2022-06-27 22:52:54 +0900

    init

---
+++ src/test/java/com/douzone/springbootswagger/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douzone.springbootswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
